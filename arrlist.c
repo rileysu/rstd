0a11901faaf65b536bdcccdf31e27cec42aaa107
Riley Sutton <riley.d.sutton@gmail.com>
2018-07-01 18:13:35 +1000

    Comments in arrlist.c

--- arrlist.c
+++ arrlist.c
@@ -5,10 +5,14 @@
 #include "arrlist.h"
 
 ArrList initArrList(int length, size_t nsize){
-	struct ArrList_t *l = malloc(sizeof(struct ArrList_t));
+	struct ArrList_t *l = malloc(sizeof(struct ArrList_t)); 
 
-	if ((l->head = malloc(nsize*length)) == NULL) 
+	//Return NULL and print error  to indicate no memory allocated
+	if ((l->head = malloc(nsize*length)) == NULL){ 
 		fprintf(stderr, "Malloc failed in initArrList.\n");
+		return NULL;
+	}
+
 	l->nsize = nsize;
 	l->clength = 0;
 	l->length = length;
@@ -23,6 +27,8 @@ void freeArrList(ArrList l){
 
 void expandArrList(int size, ArrList l){
 	void *nhead = realloc(l->head,size*l->nsize);
+	
+	//Exit after realloc failure as list requires new element when expanded
 	if (nhead == NULL){
 		fprintf(stderr, "Realloc failed in expandArrList.\n");
 		exit(-1);
@@ -32,44 +38,47 @@ void expandArrList(int size, ArrList l){
 	}
 }
 
+//NULL pointer means to allocate an empty node
 void addArrList(void *src,  ArrList l){
-	if (l->clength < l->length){
-		if (src != NULL)
-			memcpy((char*)l->head + l->clength*l->nsize,src,l->nsize);
-		l->clength++;
-	} else {
+	//Check if ArrList needs to be resized
+	if (l->clength == l->length)
 		expandArrList(l->length*2,l);
-		if (src != NULL)
-			memcpy((char*)l->head + l->clength*l->nsize,src,l->nsize);
-		l->clength++;
-	}
+
+	if (src != NULL)
+		memcpy((char*)l->head + l->clength*l->nsize,src,l->nsize);
+
+	l->clength++;
 
 }
 
 void addAllArrList(ArrList sl, ArrList dl){
-	if (sl == dl)
-		expandArrList(sl->length*2,sl);
+	//If sl == dl then the next loop will not end since the add function increments clength by 1
+	//So we create a static clength
 	int clength = sl->clength;
+
+	//Iterate through all elements in sl and add accordingly
 	for (int i = 0; i < clength; i++){
 		addArrList((char*)sl->head + i*dl->nsize,dl);
 	}
 }
 
 void delArrList(void *src, ArrList l){
+	//Iterate through all indicies and delete those that match
 	for (int i = 0; i < l->length; i++){
 		if ( (memcmp(src,((char*)l->head + i*l->nsize),l->nsize) == 0) ){
 			remArrList(i,l);
+			//Iterate i down since now the i'th element will be different
 			i--;
 		}
 	}
 }
 
 void delAllArrList(ArrList sl, ArrList dl){
-	if (sl == dl){
-		for (int i = dl->clength - 1; i >= 0; i--){
-			remArrList(i,dl);
-		}
-	} else {
+	//Protect function from failing due to deleting all elements in the same list
+	if (sl == dl)
+		//Simply reset the ArrList by setting clength to 0
+		dl->clength = 0;
+	else {
 		for (int i = 0; i < sl->clength; i++){
 			delArrList((char*)sl->head + i*dl->nsize,dl);
 		}
@@ -77,41 +86,59 @@ void delAllArrList(ArrList sl, ArrList dl){
 }
 
 bool containsArrList(void *src, ArrList l){
+	//Iterate through all indicies and return if equal element found
 	for (int i = 0; i < l->clength; i++){
 		if (memcmp(src,l->head + i*l->nsize,l->nsize) == 0){
 			return TRUE;
 		}
 	}
+
+	//If this point is reached then no element in the list is equal and thus we return FALSE
 	return FALSE;
 }
 
 int countArrList(void *src, ArrList l){
 	int count = 0;
+
+	//Iterate through all elements in arrlist and increment count accordingly if equal
 	for (int i = 0; i < l->clength; i++){
 		if (memcmp(src,(char*)l->head + i*l->nsize,l->nsize)) count++;
 	}
+
+	//All elements tested and incremented accordingly and thus we return the count
 	return count;
 }
 
 void *getArrList(int ind, ArrList l){
+	//Simply increment the head pointer by the size * ind to find the pointer to the data
 	return (char*)l->head + ind*l->nsize;
 }
 
 void setArrList(int ind, void *src, ArrList l){
-	memcpy((char*)l->head + ind*l->nsize, src, l->nsize);
+	//Simply copy the new data from src to the corresponding pointer to the data in the ArrList
+	memcpy(getArrList(ind, l), src, l->nsize);
 }
 
+//Can only squeeze between elements and at 0, not at clength
 void squeezeArrList(int ind, void* src, ArrList l){
+	//Increment clength so we know whether to expand or not
 	l->clength++;
+
+	//Expand if required
 	if (l->clength > l->length)
 		expandArrList(l->length*2,l);
+
+	//memcpy doesn't support overlapping memory so we need to do this		
 	for (int j = l->clength - 2; j >= ind; j--)
 		if (src != NULL)
 			memcpy((char*)l->head + (j+1)*l->nsize, (char*)l->head + j*l->nsize, l->nsize);
+	
+	//Empty element at ind now set
 	setArrList(ind,src,l);
 }
 
 void remArrList(int ind, ArrList l){
+	//Memory is moved down so this will work regardless of memcpy implementation
 	memcpy((char*)l->head + ind*l->nsize, (char*)l->head + (ind+1)*l->nsize, (l->clength - ind)*l->nsize);
 	l->clength--;
 }
@@ -121,6 +148,7 @@ void *getHeadArrList(ArrList l){
 }
 
 void *getTailArrList(ArrList l){
+	//Jump to the end element pointer using clength and size
 	return (char*)l->head + (l->clength-1) * l->nsize;
 }
 
